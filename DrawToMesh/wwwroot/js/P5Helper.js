e4072f72ac67275c09a03d507ce94167d0376fd8
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-03-08 20:03:15 +0530

    draw on canvas

--- DrawToMesh/wwwroot/js/P5Helper.js
+++ DrawToMesh/wwwroot/js/P5Helper.js
@@ -4,6 +4,7 @@ let pointy = [];
 
 function CreateCanvas()
 {
+ 
     const s = (p) => {
         p.setup = function () {
             p.createCanvas(document.getElementById("drawingBoard").offsetWidth,
@@ -26,11 +27,16 @@ function CreateCanvas()
 
     let myp5 = new p5(s,"drawingBoard");
     P5Object = myp5;
-
+    pointx = [];
+    pointy = [];
 }
 
 function onDraw()
 {
+    if (pointx[0] == 0 && pointy[0] == 0) {
+        pointx.splice(0, 1);
+        pointy.splice(0, 1);
+    }
     for (let i = 0; i < pointx.length; i+=1)
     {
         P5Object.circle(pointx[i], pointy[i],40);
