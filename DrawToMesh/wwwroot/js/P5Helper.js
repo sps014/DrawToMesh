b0576c8b93d938b328e7adc0abd246c74e3bd4c1
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-03-08 19:36:46 +0530

    canvas sticked

---
+++ DrawToMesh/wwwroot/js/P5Helper.js
@@ -0,0 +1,39 @@
+﻿let P5Object = new Object();
+
+function CreateCanvas()
+{
+    const s = (p) => {
+        p.setup = function () {
+            p.createCanvas(document.getElementById("drawingBoard").offsetWidth,
+                document.getElementById("drawingBoard").offsetHeight);
+            p.background(122,0,0);
+        }
+        p.windowResized = function () {
+            p.resizeCanvas(document.getElementById("drawingBoard").offsetWidth,
+                document.getElementById("drawingBoard").offsetHeight);
+            p.background(122, 0, 0);
+
+        }
+        p.draw = function () {
+            onDraw();
+        }
+    };
+
+    let myp5 = new p5(s,"drawingBoard");
+    P5Object = myp5;
+
+}
+
+function onDraw()
+{
+    P5Object.circle(12, 20, 40);
+}
+
+function onMouseClick()
+{
+
+}
+
+function onDoubleClick() {
+
+}
