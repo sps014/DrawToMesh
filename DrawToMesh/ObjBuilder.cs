149eb2c78cb0506ac48f66af54c92bb0c79bd957
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-03-14 21:14:06 +0530

    obj exporter

---
+++ DrawToMesh/ObjBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawToMesh
+{
+    public class ObjBuilder
+    {
+        public string Export(Point[] vertex,Triangle[] triangles)
+        {
+            string exported="";
+
+            exported += GetVertexData(vertex);
+            exported += GetTriangleData(triangles);
+            return exported;
+        }
+
+        private string GetVertexData(Point[] vertex)
+        {
+            StringBuilder vert = new StringBuilder("");
+            for (int i = 0; i < vertex.Length; i++)
+            {
+                vert.Append("v " + vertex[i].X + " " + vertex[i].Y + " " + vertex[i].Z+"\r\n");
+            }
+            return vert.ToString();
+        }
+
+        private string GetTriangleData(Triangle[] trigs)
+        {
+            StringBuilder trig = new StringBuilder("");
+
+            //1 based indexing 
+            for (int i = 0; i < trigs.Length; i++)
+            {
+                trig.Append("f " + (trigs[i].P1 + 1) + " " + (trigs[i].P2 + 1) + " " + (trigs[i].P3 + 1)+"\r\n");
+            }
+
+            return trig.ToString();
+        }
+    }
+}
---
+++ DrawToMesh/Object3D.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrawToMesh
+{
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Point() { }
+        public Point(double X,double Y,double Z)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+        }
+    }
+    public class Triangle
+    {
+        /// <summary>
+        /// index
+        /// </summary>
+        public int P1 { get; set; }
+        /// <summary>
+        /// index p2 
+        /// </summary>
+        public int P2 { get; set; }
+        /// <summary>
+        /// index p3
+        /// </summary>
+        public int P3 { get; set; }
+        /// <summary>
+        /// pass indexea
+        /// </summary>
+        /// <param name="p1">index p1</param>
+        /// <param name="p2"></param>
+        /// <param name="p3"></param>
+        public Triangle(int p1, int p2, int p3)
+        {
+            this.P1 = p1;
+            this.P2 = p2;
+            this.P3 = p3;
+        }
+
+    }
+}
