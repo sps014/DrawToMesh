a99aadd84123dbf610302720bca6b2da91b78813
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-03-14 16:20:39 +0530

    lol

--- DrawToMesh/wwwroot/js/preview3D.js
+++ DrawToMesh/wwwroot/js/preview3D.js
@@ -1,9 +1,10 @@
 ﻿let P5;
 let canvas3d;
 let gridGap = 10;
+let mode;
+function CreateCanvas3D(Mode=0) {
 
-function CreateCanvas3D() {
-
+    mode = Mode;
     canvas3d = document.getElementById("p3dboard");
 
     const s = (p) => {
@@ -28,7 +29,15 @@ function CreateCanvas3D() {
     P5 = myp5;
 }
 
-function Draw3D() {
+function Draw3D()
+{
+    if (mode == 0) {
+        DrawFromShape();
+    }
+}
+
+function DrawFromShape()
+{
     P5.stroke(0, 255, 0);
     //P5.strokeWeight(5);
 
@@ -37,37 +46,7 @@ function Draw3D() {
     P5.beginShape(P5.TRIANGLES);
     let midX = calMidX();
     let midY = calMidY();
-    //for (let i = 0; i < pointx.length / 2; i++)
-    //{
-    //    //if (i == 0) {
-    //    //    P5.vertex(pointx[i] - midX, pointy[i] - midY, 0);
-    //    //    P5.vertex(pointx[i + 1] - midX, pointy[i + 1] - midY, 0);
-    //    //    P5.vertex(pointx[pointy.length - 2 - i] - midX, pointy[pointy.length - 2 - i] - midY, 0);
-    //    //}
-    //    if (i != pointx.length/2-1)
-    //    {
-    //        P5.vertex(pointx[i] - midX, pointy[i] - midY, 0);
-    //        P5.vertex(pointx[i+1] - midX, pointy[i+1] - midY, 0);
-    //        P5.vertex(pointx[pointy.length - 2 - i] - midX, pointy[pointy.length - 2 - i] - midY, 0);
 
-    //        P5.vertex(pointx[i + 1] - midX, pointy[i + 1] - midY, 0);
-    //        P5.vertex(pointx[pointy.length - 2 - i] - midX, pointy[pointy.length - 2 - i] - midY, 0);
-    //        P5.vertex(pointx[pointy.length - 3 - i] - midX, pointy[pointy.length - 3 - i] - midY, 0);
-
-
-    //        P5.vertex(pointx[i] - midX, pointy[i] - midY, 40);
-    //        P5.vertex(pointx[i + 1] - midX, pointy[i + 1] - midY, 40);
-    //        P5.vertex(pointx[pointy.length - 2 - i] - midX, pointy[pointy.length - 2 - i] - midY, 40);
-
-    //        P5.vertex(pointx[i + 1] - midX, pointy[i + 1] - midY, 40);
-    //        P5.vertex(pointx[pointy.length - 2 - i] - midX, pointy[pointy.length - 2 - i] - midY, 40);
-    //        P5.vertex(pointx[pointy.length - 3 - i] - midX, pointy[pointy.length - 3 - i] - midY, 40);
-
-    //    }
-
-
-
-    //}
     let parr = [];
     for (let i = 0; i < pointx.length - 1; i++) {
         parr.push(pointx[i]);
@@ -77,15 +56,15 @@ function Draw3D() {
     let res = earcut(parr);
     for (let i = 0; i < res.length; i += 3) {
         P5.vertex(pointx[res[i]] - midX, pointy[res[i]] - midY);
-        P5.vertex(pointx[res[i + 1]] - midX, pointy[res[i + 1]]-midY);
+        P5.vertex(pointx[res[i + 1]] - midX, pointy[res[i + 1]] - midY);
         P5.vertex(pointx[res[i + 2]] - midX, pointy[res[i + 2]] - midY);
 
     }
 
 
 
-    for (let i = 0; i < pointx.length-1; i++) {
-       
+    for (let i = 0; i < pointx.length - 1; i++) {
+
 
         P5.vertex(pointx[i] - midX, pointy[i] - midY, 0);
         P5.vertex(pointx[i + 1] - midX, pointy[i + 1] - midY, 0);
@@ -96,7 +75,7 @@ function Draw3D() {
         P5.vertex(pointx[i + 1] - midX, pointy[i + 1] - midY, 40);
 
 
-       
+
         //P5.vertex(pointx[i] - calMidX(), pointy[i] - calMidY(), 0);
         //P5.vertex(pointx[i] - calMidX(), pointy[i] - calMidY(), 40);
 
@@ -125,81 +104,3 @@ function Remove3D() {
     P5.remove();
 }
 
-function createPolygon()
-{
-    let poly=[];
-    for (let i = 0; i < pointx.length-1; i++)
-    {
-        poly.push({ x: pointx[i], y: pointy[i] })
-    }
-    return poly;
-}
-
-function cross(a,b)
-{
-    return a.x * b.y - a.y + b.x;
-}
-function orien(polygon)
-{
-    let sum = 0;
-    for (let i = 0; i < polygon.length-1; i++)
-    {
-        sum += cross(polygon[i], polygon[i + 1] );
-    }
-    return sum;
-}
-function GetAllTrigs()
-{
-    let poly = createPolygon();
-    let isCW = (orien(poly) > 0);
-    if (!isCW)
-    {
-        poly.reverse();
-    }
-
-    let Trigs = [];
-
-    while (poly.length >= 3)
-    {
-        let l = poly.length;
-        let trigRemoved = false;
-
-        for (let i = 0; i < l; i++)
-        {
-            let p1 = poly[i];
-            let p2 = poly[(i+1)%l];
-            let p3 = poly[(i + 2) % l];
-
-            let cw = (orien([p1, p2, p3]) > 0);
-            if (!cw)
-                continue;
-
-            let hasPoint = CheckIfContainPoly(p1, p2, p3, poly);
-            if (hasPoint)
-                continue;
-            trigRemoved = true;
-            Trigs.push({ p1: p1, p2: p2,p3: p3 });
-
-            poly.splice((i + 1) % l, 1);
-        }
-
-        if (!trigRemoved)
-            break;
-    }
-    return Trigs;
-
-}
-
-function CheckIfContainPoly(p1, p2, p3, poly)
-{
-    for (let i = 0; i < poly.length; i++)
-    {
-        if (poly[i] == p1 || poly[i] == p2 || poly[i] == p3)
-            continue;
-        return (orien([poly[i], p1, p2]) > 0 && orien([poly[i], p2, p3]) > 0
-            && orien([poly[i], p3, p1]) > 0);
-       
-    }
-
-    return false;
-}
