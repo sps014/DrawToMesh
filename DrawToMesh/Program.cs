3191d8f8ce301a23cbca409200365382b403d766
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-06-18 13:53:00 +0530

    .net 6 preview 5 update

--- DrawToMesh/Program.cs
+++ DrawToMesh/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http;
 
 namespace DrawToMesh
 {
@@ -12,8 +13,10 @@ namespace DrawToMesh
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-            builder.Services.AddBaseAddressHttpClient();
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
             await builder.Build().RunAsync();
         }
     }
